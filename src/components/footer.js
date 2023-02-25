cd89d0e0412ef68fd9bb84a1a9cbd981c6c172c6
KatsuyaHiguchi <katsuya-higuchi@nekonote-software.com>
2023-02-25 23:46:56 +0900

    Finished Gatsby Tutorial Part 2

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () =>{
+    return(
+        <footer>
+            <div>
+                Copyright © 2021 tomoki.dojo
+            </div>
+        </footer>
+    )
+}
+export default Footer //Footer関数をエクスポート
---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () =>{
+    return(
+        <header>
+            <div>
+                <div>
+                    クマの生態ブログ
+                </div>
+            </div>
+        </header>
+    )
+}
+export default Header //Header関数をエクスポート。詳しくは後述。
